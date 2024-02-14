e58f544265a94a313ce01cad432064d76fa1e519
Felipe <felipdetavares@gmail.com>
2024-02-14 20:44:52 -0300

    feat: Immutability

---
+++ Immutability/index.js
@@ -0,0 +1,8 @@
+// Retornará uma errro pois não podem haver duas variáveis com o mesmo nome no mesmo bloco.
+
+// "Em termos de mutabildiade variáveis declaradas com 'let' podem ter seu valor alterado depois de ter sido inicializada.
+// Enquanto que o mesmo não acontece com as declaradas com 'const', não sendo possível reatribuir o seu valor depois inicializada
+
+let nome = "Felipe Tavares";
+
+const nome = "Felipe Tavares";
